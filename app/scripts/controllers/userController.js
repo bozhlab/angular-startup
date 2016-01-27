6799e5b8165763b8c996c32bca40edb71917c03d
Suthiwat <suthiwat@iconext.co.th>
2016-01-27 18:19:23 +0700

    Repeat data with search filter

--- app/scripts/controllers/userController.js
+++ app/scripts/controllers/userController.js
@@ -1,3 +1,25 @@
 myApp.controller('userController', ['$scope', function ($scope) {
     $scope.message = 'hello';
+    $scope.users = [
+        {
+            'name': 'Anthony "Tony" Edward',
+            'lastName': 'Stark'
+        },
+        {
+            'name': 'Thor',
+            'lastName': 'Odinson'
+        },
+        {
+            'name': 'Henry Jonathan "Hank"',
+            'lastName': 'Pym'
+        },
+        {
+            'name': 'Robert Bruce',
+            'lastName': 'Banner'
+        },
+        {
+            'name': 'Janet van',
+            'lastName': 'Dyne'
+        }
+  ];
 }]);
