81e217b54bad7f85970644c8ccef9c2aac81cf95
Suthiwat <suthiwat@iconext.co.th>
2016-01-28 14:10:42 +0700

    Doing with Route and Template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,12 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+    when('/users', {
+        templateUrl: 'views/user/index.html',
+        controller: 'userController'
+    }).
+    otherwise({
+        redirectTo: '/users'
+    });
+  }]);
